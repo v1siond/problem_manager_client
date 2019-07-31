901d3fe1f22d8c91f89dbd0fc01f5683a57bcfeb
v1siond <alexanderpulido81@gmail.com>
2019-07-30 21:50:54 -0500

    add router and basic router config. ----This is for the last commit -> Improve folders order----

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from '../static/images/logo.svg';
-import '../styles/App.css';
-
+import React from 'react'
+import '../styles/App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from '../pages/Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
