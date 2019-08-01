9d4ca872ed61137c39b3b54d1aa2f7189dd9b351
v1siond <alexanderpulido81@gmail.com>
2019-08-01 09:12:45 -0500

    add fallback to env

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { setContext } from 'apollo-link-context'
 
 
 const httpLink = new HttpLink({
-  uri: `${process.env.REACT_APP_API_BASE_URL}/graphql`
+  uri: `${process.env.REACT_APP_API_URL || 'http://localhost:3000'}/graphql`
 })
 
 const authLink = setContext((_, { headers }) => {
