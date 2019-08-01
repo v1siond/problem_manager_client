11560d9526578169a710ac3591e30efd061a2aec
v1siond <alexanderpulido81@gmail.com>
2019-08-01 08:35:05 -0500

    use react_app convenion in index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { setContext } from 'apollo-link-context'
 
 
 const httpLink = new HttpLink({
-  uri: `${process.env.API_BASE_URL}/graphql`
+  uri: `${process.env.REACT_APP_API_BASE_URL}/graphql`
 })
 
 const authLink = setContext((_, { headers }) => {
