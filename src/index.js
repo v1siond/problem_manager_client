58db9fa00792f599f2ee6326342632f2a7956658
v1siond <alexanderpulido81@gmail.com>
2019-08-01 08:26:37 -0500

    use env in uri

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { setContext } from 'apollo-link-context'
 
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/graphql'
+  uri: `${process.env.API_BASE_URL}/graphql`
 })
 
 const authLink = setContext((_, { headers }) => {
